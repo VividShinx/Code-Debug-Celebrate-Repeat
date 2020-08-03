6268953a56687d6305dd0b3aa7bbf11109bb2960
VividShinx <67226720+VividShinx@users.noreply.github.com>
2020-08-03 10:07:49 +0800

    Added metric conversion
    Added metric button and conversion javascript

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function wrapFix() {
   let citySwap = document.querySelector(".search-Button");
   citySwap.addEventListener("click", showCity);
 
-  showTemp("Melbourne");
+  showTemp("Brisbane");
 }
 
 function dayDisplay(date) {
@@ -45,22 +45,66 @@ function showTemp(userInput) {
   axios.get(`${apiUrl}&appid=${apiKey}`).then(typeHere);
 }
 
+function metricChange() {
+  let tempval = document.querySelector("#tempSection");
+  let theRestOfIt = tempval.innerHTML.slice(0, -2);
+  let theLastCharacter = tempval.innerHTML.slice(-1)[0]; //Get the C or the F
+  if (theLastCharacter == "F") {
+    tempval.innerHTML = `${theRestOfIt}°C`;
+  } else {
+    tempval.innerHTML = `${theRestOfIt}°F`;
+  }
+  let theCity = document.querySelector("#cityChange").innerHTML;
+  showTemp(theCity);
+}
+
 function typeHere(response) {
   let temperature = Math.round(response.data.main.temp);
-  console.log(response.data.main.temp);
   let tempElem = document.querySelector(".temp");
-  tempElem.innerHTML = `${temperature}°C`;
+  let theRestOfIt = tempElem.innerHTML.slice(0, -2);
+  let theLastCharacter = tempElem.innerHTML.slice(-1)[0]; //Get the C or the F
+  if (theLastCharacter == "F") {
+    temperature = Math.round(temperature * (9 / 5) + 32);
+    tempElem.innerHTML = `${temperature}°F`;
+  } else {
+    tempElem.innerHTML = `${temperature}°C`;
+  }
+
   let descript = document.querySelector("#descriptWeather");
   descript.innerHTML = response.data.weather[0].description;
 
-  document.querySelector("#wind").innerHTML =
-    "Wind: " + Math.round(response.data.wind.speed) + "KM/H";
+  let winSpeed = Math.round(response.data.wind.speed);
+  if (theLastCharacter == "F") {
+    winSpeed = Math.round(winSpeed / 1.609);
+    document.querySelector("#wind").innerHTML = `${winSpeed} MI/H`;
+  } else {
+    document.querySelector("#wind").innerHTML = `${winSpeed} KM/H`;
+  }
   document.querySelector("#Humid").innerHTML =
     "Humidity: " + Math.round(response.data.main.humidity) + "%";
-  document.querySelector("#minTemp").innerHTML =
-    "Minimum Temperature: " + Math.round(response.data.main.temp_min) + "°C";
-  document.querySelector("#maxTemp").innerHTML =
-    "Maximum Temperature: " + Math.round(response.data.main.temp_max) + "°C";
+
+  let minTemp = Math.round(response.data.main.temp_min);
+  if (theLastCharacter == "F") {
+    minTemp = Math.round(minTemp * (9 / 5) + 32);
+    document.querySelector(
+      "#minTemp"
+    ).innerHTML = `Minimum Temperature: ${minTemp}°F`;
+  } else {
+    document.querySelector(
+      "#minTemp"
+    ).innerHTML = `Minimum Temperature: ${minTemp}°C`;
+  }
+  let maxTemp = Math.round(response.data.main.temp_max);
+  if (theLastCharacter == "F") {
+    maxTemp = Math.round(maxTemp * (9 / 5) + 32);
+    document.querySelector(
+      "#maxTemp"
+    ).innerHTML = `Maximum Temperature: ${maxTemp}°F`;
+  } else {
+    document.querySelector(
+      "#maxTemp"
+    ).innerHTML = `Maximum Temperature: ${maxTemp}°C`;
+  }
   let iconElement = document.querySelector("#iconPic");
   iconElement.setAttribute(
     "src",
